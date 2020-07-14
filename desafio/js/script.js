344f7ea34360dbf854914ce5eadb20a36ecf99c8
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-14 11:52:49 -0300

    Capturando valor do input

--- desafio/js/script.js
+++ desafio/js/script.js
@@ -1,24 +1,34 @@
-window.addEventListener('load', start);
+window.addEventListener("load", start);
 
-var globalNames = ['name', 'name2', 'name3'];
+var globalNames = ["name", "name2", "name3"];
 var inputName = null;
 
-function start(){
-    inputName = document.querySelector('#name');
-    
-    preventFormSubmit();
-    activateInput();
+//Carrega eventos no start da pagina
+function start() {
+  inputName = document.querySelector("#name");
+
+  preventFormSubmit();
+  activateInput();
 }
 
-function preventFormSubmit(){
-    function handleFormSubmit(event){
-        event.preventDefault();
+//Previne o reload da tecla 'enter'
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  var form = document.querySelector("form");
+  form.addEventListener("submit", handleFormSubmit);
+}
+
+//Funcao que dá foco ao campo e captura escrita
+function activateInput() {
+  function handleTyping(event) {
+    if (event.key === "Enter") {
+      console.log(event.target.value);
     }
+  }
 
-    var form = document.querySelector('form');
-    form.addEventListener('submit', handleFormSubmit);
-}
-
-function activateInput(){
-    inputName.focus();
+  inputName.focus();
+  inputName.addEventListener("keyup", handleTyping);
 }

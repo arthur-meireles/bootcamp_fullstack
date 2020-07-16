3ab18dc6ebc9dbaadd7fdc65284ed4da7fff4a0f
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-16 12:05:49 -0300

    Adicionando funcionalidade de edição

--- desafio/js/script.js
+++ desafio/js/script.js
@@ -7,6 +7,8 @@ var globalNames = [
   "Laurrane Maira Ferreira",
 ];
 var inputName = null;
+var isEditing = false;
+var currentIndex = null;
 
 //Carrega eventos no start da pagina
 function start() {
@@ -15,6 +17,8 @@ function start() {
   activateInput();
   render();
 }
+
+//Tratando vazio
 
 //Previne o reload da tecla 'enter'
 function preventFormSubmit() {
@@ -26,7 +30,7 @@ function preventFormSubmit() {
   form.addEventListener("submit", handleFormSubmit);
 }
 
-//Funcao que dá foco ao campo e captura escrita
+//Funcao que captura escrita e da foco ao campo
 function activateInput() {
   function insertName(newName) {
     globalNames.push(newName);
@@ -34,11 +38,23 @@ function activateInput() {
     render();
   }
 
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+    
+  }
   function handleTyping(event) {
     if (event.key === "Enter") {
-      var typedName = event.target.value;
-      insertName(typedName);
+      if (isEditing === true) {
+        updateName(event.target.value);
+      } else {
+        var typedName = event.target.value;
+        insertName(typedName);
+      }
+      isEditing = false;
+      clearInput();
+      render();
     }
+   
   }
 
   inputName.addEventListener("keyup", handleTyping);
@@ -51,27 +67,48 @@ function render() {
   table.innerHTML =
     '<th class="theader">Contatos</th><th class="theader">Ações</th>';
 
-  //Criando Botao
-  function createButton(tr) {
+  //Criando table-row com td dentro e botão
+  function createTr(currentName, index) {
+    //Criando Botao de Delete
+    function createDeleteButton() {
+      function deleteName() {
+        globalNames.splice(index, 1);
+        render();
+      }
+      var span = document.createElement("span");
+      var td2 = document.createElement("td");
+      span.innerHTML = '<span class="material-icons">delete_forever</span>';
+      td2.appendChild(span);
+      tr.appendChild(td2);
+
+      span.addEventListener("click", deleteName);
+    }
+
+    //tornando editavel
+    function editItem() {
+      inputName.value = currentName;
+      isEditing = true;
+      currentIndex = index;
+      inputName.focus();
+    }
+
+    //Montando Lista
+    var tr = document.createElement("tr");
     var td = document.createElement("td");
-    var span = document.createElement("span");
-    span.innerHTML = '<span class="material-icons">delete_forever</span>';
-    td.appendChild(span);
-    tr.appendChild(td);
+    var name = document.createTextNode(currentName);
+    td.addEventListener("click", editItem); // Adicionando evento ao elemento td
+
+    td.appendChild(name); // td escreve o name dentro
+    tr.appendChild(td); //tr monta um td dentro
+    createDeleteButton(); //Funcao escreve o botao (recebe elemento pai)
+    table.appendChild(tr); // table monta o tr dentro
   }
 
   for (var i = 0; i < globalNames.length; i++) {
     var currentName = globalNames[i];
 
-    var tr = document.createElement("tr");
-    var td = document.createElement("td");
-    var name = document.createTextNode(currentName);
-
-    //Montando Lista
-    tr.appendChild(td); //tr monta um td dentro
-    td.appendChild(name); // td escreve o name dentro
-    createButton(tr); //Funcao escreve o botao (recebe elemento pai)
-    table.appendChild(tr); // table monta o tr dentro
+    //Montando lista
+    createTr(currentName, i);
   }
   clearInput();
 }

bc47d0e9adaf5c5c42ffdbd16965facb93dfbcfd
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-17 11:27:43 -0300

    Capturando o valor dos ranges e exibindo

--- RGB/js/script.js
+++ RGB/js/script.js
@@ -1,9 +1,34 @@
+//Evento que captura o load da pagina
 window.addEventListener("load", start);
 
 //GLOBAL VARIABLES
-
+var range = null;
+var number = null;
 
 //Carrega eventos no start da pagina
 function start() {
+  range = document.querySelectorAll(".range");
+
+  number = document.querySelectorAll(".number");
+
+  //Funções
+  writeValue();
+}
+
+function writeValue() {
+
+
+  function handleValue(event) {
+    console.log(event);
+    for(var i = 0; i < range.length; i++){
+      number[i].value = range[i].value;
+      console.log(number[i].value);
+    }
+  
+  }
+
+  for (var i = 0; i < range.length; i++) {
+    range[i].addEventListener("change", handleValue);
+  }
 
 }

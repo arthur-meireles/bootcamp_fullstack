cf73767bcb12311a1c3bc33569ebad0ac925f19b
Arthur Meireles <arthurmeireles@MacBook-Pro.local>
2020-07-17 12:02:40 -0300

    Finalizando trabalho prático

--- RGB/js/script.js
+++ RGB/js/script.js
@@ -4,31 +4,36 @@ window.addEventListener("load", start);
 //GLOBAL VARIABLES
 var range = null;
 var number = null;
+var square = null;
 
 //Carrega eventos no start da pagina
 function start() {
   range = document.querySelectorAll(".range");
-
   number = document.querySelectorAll(".number");
+  square = document.querySelector(".square");
 
   //Funções
-  writeValue();
+  colorfy();
 }
 
-function writeValue() {
+function colorfy() {
 
-
-  function handleValue(event) {
-    console.log(event);
-    for(var i = 0; i < range.length; i++){
+  //Captura valor do range
+  function writeValue() {
+    for (var i = 0; i < range.length; i++) {
       number[i].value = range[i].value;
-      console.log(number[i].value);
     }
-  
+
+    function handleColor() {
+      var color = `rgb(${number[0].value}, ${number[1].value}, ${number[2].value})`;
+      var shadow = `0px 0px 79px 10px rgba(${number[0].value}, ${number[1].value}, ${number[2].value}, 1)`;
+      square.style.backgroundColor = color;
+      square.style.boxShadow = shadow;
+    }
+    handleColor();
   }
 
   for (var i = 0; i < range.length; i++) {
-    range[i].addEventListener("change", handleValue);
+    range[i].addEventListener("change", writeValue);
   }
-
 }

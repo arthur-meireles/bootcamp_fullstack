81126a5b4b3c08a916e9f307664c16fa99ba4f3e
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-07-24 10:04:05 -0300

    Somando população total com reduce() no renderSummary()

--- DG-02/js/script.js
+++ DG-02/js/script.js
@@ -24,12 +24,12 @@ window.addEventListener('load', () => {
 	//Mapeando a DOM
 	tabCountries = document.querySelector('.tabCountries');
 	tabFavorites = document.querySelector('.tabFavorites');
-	countCountries = document.querySelector('.countCountries');
-	countFavorites = document.querySelector('.countFavorites');
-	totalPopulationList = document.querySelector('.totalPopulationList');
+	countCountries = document.querySelector('#countCountries');
+	countFavorites = document.querySelector('#countFavorites');
+	totalPopulationList = document.querySelector('#totalPopulationList');
 	//prettier-ignore
 	totalPopulationFavorites = 
-    document.querySelector('.totalPopulationFavorites');
+    document.querySelector('#totalPopulationFavorites');
 
 	numberFormat = Intl.NumberFormat('pt-BR');
 
@@ -51,7 +51,7 @@ async function fetchCountries() {
 			population,
 			flag
 		};
-	});
+    });
 
 	//Montando a tela
 	render();
@@ -65,7 +65,7 @@ function render() {
 }
 
 function renderCountrieList() {
-	let countriesHTML = '<div>';
+	let countriesHTML = '';
 	allCountries.forEach(country => {
 		const { name, population, flag, id } = country;
 
@@ -83,7 +83,10 @@ function renderCountrieList() {
             </div>
 
             <div class="nome">
-                ${name}
+                <ul>
+                    <li>${name}</li>
+                    <li>${population}</li>
+                </ul>
             </div>
 
             </div>
@@ -92,6 +95,48 @@ function renderCountrieList() {
 	});
 	tabCountries.innerHTML = countriesHTML;
 }
-function renderFavorites() {}
-function renderSummary() {}
+
+function renderFavorites() {
+    let favoritesHTML = '';
+
+    favoriteCountries.forEach(country =>{
+        const {name, population, flag, id} = country;
+
+        const favoriteCountryHTML = `
+            <div class="country">
+                <div>
+                    <a id=${id} class="material-icons">
+                    remove_circle_outline
+                    </a>
+                </div>
+                <div class="flag">
+                    <img src="${flag}" alt="${name}">
+                </div>
+                <div class="nome">
+                    <ul>
+                        <li>${name}</li>
+                        <li>${population}</li>
+                    </ul>
+                </div>
+            </div>
+        `;
+		favoritesHTML += favoriteCountryHTML;
+	});
+    tabFavorites.innerHTML = favoritesHTML;
+    }
+
+function renderSummary() {
+    //Total países
+    countCountries.textContent = allCountries.length;
+    countFavorites.textContent = favoriteCountries.length;
+
+    //Somando a populacao
+    const totalPopulation = allCountries.reduce((accumulator, current)=>{
+        return accumulator + current.population
+    }, 0);
+
+    totalPopulationList.textContent = totalPopulation;
+
+}
+
 function handleCountrieButtons() {}

792bdfef9a95700867567c8dae32287364ef214b
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-07-24 20:57:23 -0300

    Transformando os dados em array

--- Desafio/js/script.js
+++ Desafio/js/script.js
@@ -1,6 +1,33 @@
+/**
+ *          ESTADO DA APLICAÇÃO
+ * **/
+
+let allUsers = [];
+
 window.addEventListener('load', () => {
-    //Implementa variaveise
+	//Implementa variaveise
 
-
-    //Funcoes
+	//Funcoes
+	fetchUsers();
 });
+
+function fetchUsers() {
+    allUsers = user.map(user => {
+        const name = `${user.name.first} ${user.name.last}`;
+        const {gender} = user;
+        return {
+            name,
+            age: user.dob.age,
+            gender
+        };
+    }).sort((a, b) => a.name.localeCompare(b.name))
+
+    console.log(allUsers);
+    render();
+}
+
+
+function render() {
+    renderUserList();
+}
+

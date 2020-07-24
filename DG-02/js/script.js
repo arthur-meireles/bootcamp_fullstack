9a0ce25d6d65cbe06fc3c260ba7b257fbd0672a5
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-07-24 08:33:57 -0300

    criando estado da aplicação e Mapeando a DOM

---
+++ DG-02/js/script.js
@@ -0,0 +1,42 @@
+/*
+********* ESTADO DA APLICAÇÃO [state] *********
+*/
+
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCountries = [];
+let favoriteCountries = [];
+
+let countCountries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFavorites = 0;
+
+let numberFormat = null;
+
+/*
+********* ON LOAD *********
+*/
+
+window.addEventListener('load', () => {
+	//Mapeando a DOM
+	tabCountries = document.querySelector('.tabCountries');
+	tabFavorites = document.querySelector('.tabFavorites');
+	countCountries = document.querySelector('.countCountries');
+	countFavorites = document.querySelector('.countFavorites');
+	totalPopulationList = document.querySelector('.totalPopulationList');
+	//prettier-ignore
+	totalPopulationFavorites = 
+    document.querySelector('.totalPopulationFavorites');
+
+	numberFormat = Intl.NumberFormat('pt-BR');
+
+    //Invocando funções
+	fetchCountries();
+});
+
+function fetchCountries() {
+	console.log('fetching countries...');
+}

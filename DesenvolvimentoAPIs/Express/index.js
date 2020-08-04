c5a9bb15059e22153771f80ec9ce8411c9942bbe
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-04 09:17:29 -0300

    Caracteres especeiais em rotas

--- DesenvolvimentoAPIs/Express/index.js
+++ DesenvolvimentoAPIs/Express/index.js
@@ -7,7 +7,7 @@ app.all('/testAll', (req, res) => {
 });
 
 //Aceita /teste ou /test
-app.get('/test?', (_,res)=>{
+app.get('/tester?', (_,res)=>{
     res.send('a')
 });
 
@@ -15,6 +15,17 @@ app.get('/test?', (_,res)=>{
 app.get('/buzz+', (_,res)=>{
     res.send('Foi')
 });
+
+//Aceita qualquer coisa no lugar de *
+app.get('/one*blue', (req,res)=>{
+    res.send(req.path);
+});
+
+//Aceita qualquer coisa no lugar de *
+app.get('/test(ing)?', (req,res)=>{
+    res.send('/test(ing)');
+});
+
 
 app.listen(3000, () => {
 	console.warn('API Started on port 3000...');

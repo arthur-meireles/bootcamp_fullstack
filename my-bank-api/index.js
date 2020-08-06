e41e3d2d7fbe1412ddbaa0086f5f76a6c9de023a
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-06 12:19:14 -0300

    Finalizando

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-const app = express();
+import accountsRouter from './routes/accounts.js'
 
+const app = express();
 app.use(express.json());
+
+app.use('/account', accountsRouter);
 
 
 

943665cb29a41dac0c2a5ffd95bc1369d715fc1c
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-06 16:25:10 -0300

    Cadastrando nova conta

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -1,13 +1,31 @@
 import express from 'express';
-import accountsRouter from './routes/accounts.js'
+import { promises as fs } from 'fs';
+import accountsRouter from './routes/accounts.js';
 
+const { readFile, writeFile } = fs;
+const path = './jsons';
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use('/account', accountsRouter);
 
 
 
-app.listen(3000, () => {
-    console.log('Server runing on port 3000...')
-}
+app.listen(3000, async () => {
+	try {
+		await readFile(`${path}/accounts.json`);
+		console.log('File exists');
+		console.log('Server runing on port 3000...');
+	} catch (err) {
+		const initialJson = {
+			nextId: 1,
+			accounts: [],
+		};
+		await writeFile(`${path}/accounts.json`, JSON.stringify(initialJson))
+			.then(() => {
+				console.log('File created');
+				console.log('Server runing on port 3000...');
+			})
+			.catch(err => console.log(err));
+	}
+});
--- my-bank-api/routes/accounts.js
+++ my-bank-api/routes/accounts.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+
+const path = './jsons';
 const router = express.Router();
+const { readFile, writeFile } = fs;
 
+router.post('/', async (req, res) => {
+	try {
+		let account = req.body;
+		const data = JSON.parse(await readFile(`${path}/accounts.json`));
 
+		account = { id: data.nextId++, ...account };
+		data.accounts.push(account);
 
+		await writeFile(`${path}/accounts.json`, JSON.stringify(data, null, 2));
+
+		res.send(account);
+	} catch (err) {
+		res.status(400).send({ error: err.message });
+	}
+});
 
 export default router;

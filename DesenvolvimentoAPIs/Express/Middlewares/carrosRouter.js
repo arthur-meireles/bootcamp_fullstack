1b3698d0324d7f110a9b6666d14d271e997c987f
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-06 10:17:35 -0300

    Tratamento de erros

--- DesenvolvimentoAPIs/Express/Middlewares/carrosRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-let router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send(req.method);
-})
-
-export default router;
--- DesenvolvimentoAPIs/Express/Middlewares/index.js
+++ DesenvolvimentoAPIs/Express/Middlewares/index.js
@@ -1,20 +1,25 @@
 import express from 'express';
-import carrosRouter from './carrosRouter.js';
-
 const app = express();
 app.use(express.json());
-app.use('/carros', carrosRouter);
 
-app.use((req, res, next)=>{
-    console.log(new Date());
-    next();
-})
+app.get('/', (req, res) => {
+	throw new Error('error message tst');
+});
 
+app.post('/', async (req, res, next) => {
+	try {
+		throw new Error('error Async');
+	} catch (err) {
+		next(err);
+	}
+});
 
-app.get('/test',  (req, res) => {
-    res.end();
-})
+//Bom se manter no final
+app.use((err, req, res, next) => {
+	console.log('erro 1');
+	res.status(500).send('AI CARALHO, UM ERRO!');
+});
 
-app.listen(3000, ()=>{
-    console.log('API Started...')
+app.listen(3000, () => {
+	console.log('API Started...');
 });

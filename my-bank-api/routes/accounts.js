47528faa892c572bac8caccf571ec90bb1e7b194
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-07 11:04:52 -0300

    Finalizando validaçoes de campos

--- my-bank-api/routes/accounts.js
+++ my-bank-api/routes/accounts.js
@@ -6,16 +6,26 @@ const router = express.Router();
 const { readFile, writeFile } = fs;
 
 // post account
-router.post('/', async (req, res,next) => {
+router.post('/', async (req, res, next) => {
 	try {
 		let account = req.body;
+		//Tratando vazio
+		if (!account.name || account.balance == null) {
+			throw new Error('account name or account balance not defined.');
+		}
+
 		const data = JSON.parse(await readFile(`${global.path}`));
 
-		account = { id: data.nextId++, ...account };
+		account = {
+			id: data.nextId++,
+			name: account.name,
+			balance: account.balance,
+		};
+
 		data.accounts.push(account);
 
 		await writeFile(`${global.path}`, JSON.stringify(data, null, 2));
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send(account);
 	} catch (err) {
 		next(err);
@@ -23,11 +33,11 @@ router.post('/', async (req, res,next) => {
 });
 
 // get all accounts
-router.get('/', async (req, res,next) => {
+router.get('/', async (req, res, next) => {
 	try {
 		const data = JSON.parse(await readFile(`${global.path}`));
-        delete data.nextId;
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		delete data.nextId;
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send(data);
 	} catch (err) {
 		next(err);
@@ -35,12 +45,12 @@ router.get('/', async (req, res,next) => {
 });
 
 //get account by id
-router.get('/:id', async (req, res,next) => {
+router.get('/:id', async (req, res, next) => {
 	try {
 		const data = JSON.parse(await readFile(`${global.path}`));
 		const id = req.params.id;
-        let account = data.accounts.find(account => account.id == id);
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		let account = data.accounts.find(account => account.id == id);
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send(account);
 	} catch (err) {
 		next(err);
@@ -48,14 +58,14 @@ router.get('/:id', async (req, res,next) => {
 });
 
 //delete account
-router.delete('/:id', async (req, res,next) => {
+router.delete('/:id', async (req, res, next) => {
 	try {
 		const data = JSON.parse(await readFile(`${global.path}`));
 		data.accounts = data.accounts.filter(
 			account => account.id != req.params.id,
 		);
-        await writeFile(`${global.path}`, JSON.stringify(data, null, 2));
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		await writeFile(`${global.path}`, JSON.stringify(data, null, 2));
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send({ message: 'Deleted with sucess!' });
 	} catch (err) {
 		next(err);
@@ -63,15 +73,25 @@ router.delete('/:id', async (req, res,next) => {
 });
 
 //update account
-router.put('/', async (req, res,next) => {
+router.put('/', async (req, res, next) => {
 	try {
 		let body = req.body;
 		const data = JSON.parse(await readFile(`${global.path}`));
 		const index = data.accounts.findIndex(account => account.id == body.id);
+		//Tratando id nao encontrado
+		if (index === -1) {
+			throw new Error('Registry not found.');
+		}
+		//Tratando vazio
+		if (!body.name || body.balance == null) {
+			throw new Error('account name or account balance not defined.');
+		}
 
-		data.accounts[index] = body;
-        await writeFile(global.path, JSON.stringify(data));
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		data.accounts[index].name = body.name;
+		data.accounts[index].balance = body.balance;
+
+		await writeFile(global.path, JSON.stringify(data, null, 2));
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send({ message: 'Update sucess!' });
 	} catch (err) {
 		next(err);
@@ -79,15 +99,18 @@ router.put('/', async (req, res,next) => {
 });
 
 //update balance
-router.patch('/updateBalance', async (req, res,next) => {
-    try {
+router.patch('/updateBalance', async (req, res, next) => {
+	try {
 		let body = req.body;
 		const data = JSON.parse(await readFile(`${global.path}`));
 		const index = data.accounts.findIndex(account => account.id == body.id);
 
+		if (index === -1) {
+			throw new Error('Registry not found.');
+		}
 		data.accounts[index].balance = body.balance;
-        await writeFile(global.path, JSON.stringify(data));
-        global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`)
+		await writeFile(global.path, JSON.stringify(data, null, 2));
+		global.logger.info(`${req.method} ${req.baseUrl} - Sucess ✓`);
 		res.send({ message: 'Update sucess!' });
 	} catch (err) {
 		next(err);
@@ -95,9 +118,11 @@ router.patch('/updateBalance', async (req, res,next) => {
 });
 
 //tratando erros
-router.use((err,req,res,next)=>{
-    global.logger.error(`${req.method} ${req.baseUrl} - [ERROR ✗]: ${err.message}`);
-    res.status(400).send({ error: err.message });
+router.use((err, req, res, next) => {
+	global.logger.error(
+		`${req.method} ${req.baseUrl} - [ERROR ✗]: ${err.message}`,
+	);
+	res.status(400).send({ error: err.message });
 });
 
 export default router;

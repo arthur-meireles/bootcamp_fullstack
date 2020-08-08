b6576444debe57762e130a4699be27ee0a98e8f1
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-08 09:56:39 -0300

    Criando rota para update de grade

--- 02.DesenvolvimentoAPIs/05.Desafio/index.js
+++ 02.DesenvolvimentoAPIs/05.Desafio/index.js
@@ -3,6 +3,7 @@ import { promises as fs } from 'fs';
 import { grades } from './backup/gradesBackup.js';
 import winston from 'winston';
 import cors from 'cors';
+import gradesRouter from './routes/grades.js';
 
 const app = express();
 const { readFile, writeFile } = fs;
@@ -24,14 +25,16 @@ global.logger = winston.createLogger({
 
 app.use(express.json());
 app.use(cors());
+app.use('/grades', gradesRouter);
 
 app.listen(3000, async () => {
 	try {
-		await readFile(path);
-		logger.info('All right here, rising aplication...');
+		if(await readFile(path)){
+        logger.info('All right here, rising aplication...');
+        }
 	} catch (err) {
-		logger.info('Ops, file not found.');
+		logger.warn('Ops, file not found.');
 		await writeFile(path, JSON.stringify(grades, null, 2));
-		logger.info(`Let's solve it now .... Solved!`);
+		logger.warn(`Let's solve it now .... Solved!`);
 	}
 });
---
+++ 02.DesenvolvimentoAPIs/05.Desafio/routes/grades.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import { promises as fs } from 'fs';
+
+const router = express.Router();
+const { readFile, writeFile } = fs;
+
+/* ---- Create grade ---- */
+router.post('/', async (req, res, next) => {
+	try {
+		let body = req.body;
+		const data = JSON.parse(await readFile(`${global.path}`));
+		const { student, subject, type, value } = body;
+		body = {
+			id: data.nextId++,
+			student: student,
+			subject: subject,
+			type: type,
+			value: value,
+			timestamp: new Date(),
+		};
+
+		data.grades.push(body);
+		await writeFile(global.path, JSON.stringify(data, null, 2));
+		global.logger.info(
+			`${req.method} ${req.baseUrl} - Grade Created ✓ | id: ${body.id}`,
+		);
+		res.send(body);
+	} catch (err) {
+		next(err);
+	}
+});
+/* ---- Update grade ---- */
+router.put('/', async (req, res, next) => {
+	try {
+        let body = req.body;
+        const { id, student, subject, type, value } = body;
+
+        //Tratando vazio
+		if (!id || !student || !subject || !type || value == null) {
+			throw new Error('Required fields are blank.');
+        }
+        
+		const data = JSON.parse(await readFile(`${global.path}`));
+		const index = data.grades.findIndex(grade => grade.id == id);
+
+		//Tratando id nao encontrado
+		if (index === -1) {
+			throw new Error('Nothing with this id was found.');
+		}
+		
+		data.grades[index].student = student;
+        data.grades[index].subject= subject,
+        data.grades[index].type = type,
+        data.grades[index].value = value,
+        data.grades[index].timestamp = new Date(),
+
+		await writeFile(global.path, JSON.stringify(data, null, 2));
+		global.logger.info(`${req.method} ${req.baseUrl} - Grade Updated ✓ | id: ${id}`);
+		res.send({ message: 'Grade successfully changed!' });
+	} catch (err) {
+		next(err);
+	}
+});
+
+//tratando erros
+router.use((err, req, res, next) => {
+	global.logger.error(
+		`${req.method} ${req.baseUrl} - [ERROR ✗]: ${err.message}`,
+	);
+	res.status(400).send({ error: err.message });
+});
+
+export default router;

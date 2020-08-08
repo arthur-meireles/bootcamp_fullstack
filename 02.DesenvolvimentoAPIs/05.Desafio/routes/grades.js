4df110f00f703e20cbbd1108a9559a5815abb8d7
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-08 10:32:02 -0300

    Finalizando delete e comecando get by id

--- 02.DesenvolvimentoAPIs/05.Desafio/routes/grades.js
+++ 02.DesenvolvimentoAPIs/05.Desafio/routes/grades.js
@@ -29,17 +29,18 @@ router.post('/', async (req, res, next) => {
 		next(err);
 	}
 });
+
 /* ---- Update grade ---- */
 router.put('/', async (req, res, next) => {
 	try {
-        let body = req.body;
-        const { id, student, subject, type, value } = body;
+		let body = req.body;
+		const { id, student, subject, type, value } = body;
 
-        //Tratando vazio
+		//Tratando vazio
 		if (!id || !student || !subject || !type || value == null) {
 			throw new Error('Required fields are blank.');
-        }
-        
+		}
+
 		const data = JSON.parse(await readFile(`${global.path}`));
 		const index = data.grades.findIndex(grade => grade.id == id);
 
@@ -47,16 +48,65 @@ router.put('/', async (req, res, next) => {
 		if (index === -1) {
 			throw new Error('Nothing with this id was found.');
 		}
-		
-		data.grades[index].student = student;
-        data.grades[index].subject= subject,
-        data.grades[index].type = type,
-        data.grades[index].value = value,
-        data.grades[index].timestamp = new Date(),
 
+		data.grades[index].student = student;
+		(data.grades[index].subject = subject),
+			(data.grades[index].type = type),
+			(data.grades[index].value = value),
+			(data.grades[index].timestamp = new Date()),
+			await writeFile(global.path, JSON.stringify(data, null, 2));
+		global.logger.info(
+			`${req.method} ${req.baseUrl} - Grade Updated ✓ | id: ${id}`,
+		);
+		res.send({ message: `Grade with id: ${id}, was successfully changed!` });
+	} catch (err) {
+		next(err);
+	}
+});
+
+/* ---- Delete grade ---- */
+router.delete('/:id', async (req, res, next) => {
+	try {
+		let id = req.params.id;
+		let data = JSON.parse(await readFile(`${global.path}`));
+		const index = data.grades.findIndex(grade => grade.id == id);
+
+		//Tratando id nao encontrado
+		if (index === -1) {
+			throw new Error('Nothing with this id was found.');
+		}
+
+		data = data.grades.filter(grade => grade.id != id);
 		await writeFile(global.path, JSON.stringify(data, null, 2));
-		global.logger.info(`${req.method} ${req.baseUrl} - Grade Updated ✓ | id: ${id}`);
-		res.send({ message: 'Grade successfully changed!' });
+
+		global.logger.info(
+			`${req.method} ${req.baseUrl} - Grade Deleted ✓ | id: ${id}`,
+		);
+		res.send({ message: `Grade with id: ${id}, was successfully deleted!` });
+	} catch (err) {
+		next(err);
+	}
+});
+
+/* ---- Get grade by id ---- */
+router.get('/:id', async (req, res,next) => {
+	try {
+		let id = req.params.id;
+		let data = JSON.parse(await readFile(`${global.path}`));
+		const index = data.grades.findIndex(grade => grade.id == id);
+
+		//Tratando id nao encontrado
+		if (index === -1) {
+			throw new Error('Nothing with this id was found.');
+		}
+
+		data = data.grades.filter(grade => grade.id != id);
+		await writeFile(global.path, JSON.stringify(data, null, 2));
+
+		global.logger.info(
+			`${req.method} ${req.baseUrl} - Grade Deleted ✓ | id: ${id}`,
+		);
+		res.send({ message: `Grade with id: ${id}, was successfully deleted!` });
 	} catch (err) {
 		next(err);
 	}

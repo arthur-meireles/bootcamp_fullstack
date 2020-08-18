386f0919ab44d3abb3b3cbdcd547249f630f08b4
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-18 15:08:02 -0300

    Limpando e Reorganizando

--- react-salary/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar/Navbar';
-
-export default class App extends Component {
-  render() {
-    return <Navbar />;
-  }
-}

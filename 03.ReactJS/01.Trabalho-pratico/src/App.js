11b5411177b04fdb2b7d1dce156a8a069ce7d1ed
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-18 19:30:30 -0300

    Continuando layout

--- 03.ReactJS/01.Trabalho-pratico/src/App.js
+++ 03.ReactJS/01.Trabalho-pratico/src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grommet, Box } from 'grommet';
 import mainTheme from './themes/mainTheme.js';
 import AppBar from './components/Appbar/AppBar';
 import Inputs from './components/Input/Inputs.js';
 
-function App() {
-	return (
+export default class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			salary: 0,
+		};
+	}
+
+	render() {
+		return(
 		<Grommet theme={mainTheme} full>
 			<Box fill>
 				<AppBar />
 				<Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
 					<Box flex align="stretch" margin="10px">
-						<Inputs/>
+						<Inputs />
 					</Box>
 				</Box>
 			</Box>
 		</Grommet>
-	);
+		)
+	}
 }
-
-export default App;
--- 03.ReactJS/01.Trabalho-pratico/src/components/Input/Inputs.js
+++ 03.ReactJS/01.Trabalho-pratico/src/components/Input/Inputs.js
@@ -4,32 +4,41 @@ import { FormField, TextInput, Box } from 'grommet';
 export default class Inputs extends Component {
 	render() {
 		return (
-			<Box margin={{ bottom: '10px' }} direction="column">
+			<Box
+				margin={{
+					bottom: '10px',
+					top: '20px',
+					left: '30px',
+					right: '30px',
+				}}
+				direction="column"
+			>
 				<FormField label="Salário Bruto">
-					<TextInput placeholder="Insira o salário bruto aqui..." />
+					<TextInput type="number" />
 				</FormField>
 
 				<Box direction="row" gap="medium">
 					<FormField label="Base INSS" width="medium">
-						<TextInput />
+						<TextInput disabled={true} />
 					</FormField>
 
 					<FormField label="Desconto INSS" width="medium">
-						<TextInput />
+						<TextInput disabled={true} />
 					</FormField>
 
 					<FormField label="Base IRPF" width="medium">
-						<TextInput />
+						<TextInput disabled={true} />
 					</FormField>
 
 					<FormField label="Desconto IRPF" width="medium">
-						<TextInput />
+						<TextInput disabled={true} />
 					</FormField>
 				</Box>
 				<FormField label="Salário líquido">
-					<TextInput />
+					<TextInput disabled={true} />
 				</FormField>
 			</Box>
+			
 		);
 	}
 }

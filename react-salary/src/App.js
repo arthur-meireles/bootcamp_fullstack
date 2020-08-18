ab56a75faa7e6d565e03947966899a5dca585496
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-18 14:09:15 -0300

    Alterando para Material-UI

---
+++ react-salary/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import Navbar from './components/Navbar/Navbar';
+
+export default class App extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
--- react-salary/src/index.js
+++ react-salary/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
-import App from './components/app/App';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-

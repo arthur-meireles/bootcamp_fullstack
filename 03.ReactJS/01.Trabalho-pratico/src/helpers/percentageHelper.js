82648aaedb0870832cf233335b23c6eab7ffa0f3
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-22 15:20:18 -0300

    Adicionando Backend para trabalhar por cima

--- 03.ReactJS/01.Trabalho-pratico/src/helpers/percentageHelper.js
+++
@@ -1,9 +0,0 @@
-
-const percentageFrom = (salary, value) => {
-    let y = value * 100;
-    let x = salary;
-    x = y/x;
-    let arredondado = parseFloat(x.toFixed(2));
-    return arredondado;
-};
-export { percentageFrom };

2ddf609c20c326e2745140551fd6f8495ebc56e0
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-22 14:24:10 -0300

    Alterando Counter2 Para Functional component

--- 03.ReactJS/0.Aulas/02.FunctionalComponents/04.React-couter-02/src/components/Counter/Counter2.js
+++ 03.ReactJS/0.Aulas/02.FunctionalComponents/04.React-couter-02/src/components/Counter/Counter2.js
@@ -5,20 +5,19 @@ import DecrementButton from './DecrementButton';
 import Value from './Value';
 import Steps from './Steps';
 
-export default class Counter extends Component {
-handleClick = (clickType) => {
-	this.props.onCount(clickType)
-}
+export default function Counter(props) {
+	const { onCount } = props;
 
-	render() {
-		const {countValue, step} = this.props;
-		return (
-			<div className={css.counterContainer}>
-				<DecrementButton onDecrement={this.handleClick} />
-				<Value value={countValue} />
-				<IncrementButton onIncrement={this.handleClick} />
-				<Steps steps={step} />
-			</div>
-		);
-	}
+	const handleClick = clickType => {
+		onCount(clickType);
+	};
+	const { countValue, step } = props;
+	return (
+		<div className={css.counterContainer}>
+			<DecrementButton onDecrement={handleClick} />
+			<Value value={countValue} />
+			<IncrementButton onIncrement={handleClick} />
+			<Steps steps={step} />
+		</div>
+	);
 }

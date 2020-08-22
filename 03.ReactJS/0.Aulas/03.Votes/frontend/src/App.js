924823b93c3abac62879c3bb697324fbc3357640
arthur-meireles <arthur_meireles@hotmail.com.br>
2020-08-22 15:39:16 -0300

    Capturando dados da API com fetch

--- 03.ReactJS/0.Aulas/03.Votes/frontend/src/App.js
+++ 03.ReactJS/0.Aulas/03.Votes/frontend/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
+	constructor() {
+		super();
 
-    return (
-      <div>
-      Teste
-      </div>
-    );
-  }
+		this.state = {
+			candidates: [],
+		};
+		this.interval = null;
+	}
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			fetch('http://localhost:8080/votes')
+				.then(response => {
+					return response.json();
+				})
+				.then(json => {
+					this.setState({
+						candidates: json.candidates,
+					});
+				});
+		}, 1000);
+	}
+
+	render() {
+		return <div>Teste</div>;
+	}
 }
